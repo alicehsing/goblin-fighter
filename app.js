9b4f18621ca8555160c659a8aa92648dcc3341d2
Alice Hsing <alicehsing@gmail.com>
2021-12-10 12:58:41 -0800

    fixed append defeated goblin codes and added stretch goals

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
+// import functions and grab DOM elements
 import { renderGoblin } from './render-utils.js';
 
-// import functions and grab DOM elements
 const defeatedNUmberEl = document.querySelector('#defeated-number');
-// const defeatedListEl = document.querySelector('.defeated-list');
+const defeatedListEl = document.querySelector('.defeated-list');
 const playerHPEl = document.querySelector('#player-hp');
 const playerImgEl = document.querySelector('#player-img');
 const form = document.querySelector('form');
 const goblinListEl = document.querySelector('.goblin-list');
-
 // let state
 let defeatedGoblinsCount = 0;
 let playerHP = 10;
 const goblins = [
     { name: 'Riokz', hp: 2 },
-    { name: 'Madflab', hp: 4 },
-    { name: 'Dugu', hp: 3 }
+    { name: 'Madflab', hp: 3 },
 ];
-
 // set event listeners 
-// New goblin form
-// On Submit . . .
-//User has supplied a name and submitted the form
+// new goblin form
+// user has supplied a name and submitted the form; on Submit...
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
     const data = new FormData(form);
-    // make a new goblin with user input
     const goblinName = data.get('goblin-name');
-    // Make a new goblin object with that user input
+    // make a new goblin object with that user input
     const newGoblin = {
         name: goblinName,
-        HP: Math.ceil(Math.random() * 4)
+        hp: Math.ceil(Math.random() * 3)
     };
-    // - Add that object to the array of goblins in state
+    // create a new goblin with a random default name if none is provided and add them to the page
+    if (newGoblin.name === '') {
+        const randomName = ['Sharx', 'Duzz', 'Hokoiszea', 'Ponzu', 'Khinkea', 'Tamif'];
+        let i = Math.floor(Math.random() * 5);
+        newGoblin.name = randomName[i];
+    }
+    // add that object to the array of goblins in state
     goblins.push(newGoblin);
-
     displayGoblins();
 });
 
 function displayGoblins() {
-   //"update a list"
+   // "update a list"
     // clear out the list DOM
     goblinListEl.textContent = '';
     // loop through the goblins
@@ -49,25 +48,43 @@ function displayGoblins() {
         // make each goblin element clickable if it has an hp > 0
         if (goblin.hp > 0) {
             eachGoblinEl.addEventListener('click', () => {
-            //possibly decrement this goblin's HP
+              // possibly decrement this goblin's HP
                 if (Math.random() < 0.33) {
                     goblin.hp--;
                     alert('You hit ' + goblin.name + '!');
                 } else {
                     alert('You missed ' + goblin.name + '!');
                 }
-              //possibly decrement player HP
+              // possibly decrement player HP
                 if (Math.random() < 0.5) {
                     playerHP--;
                     alert(goblin.name + ' hit you' + '!');
                 } else {
                     alert(goblin.name + ' failed to hit you' + '!');
                 }
-              //possibly increment defeatedGoblins
+              // possibly increment defeatedGoblins
                 if (goblin.hp === 0) {
                     defeatedGoblinsCount++;
+                    alert(`You have killed ${goblin.name}! Great job!`);
+                    // add defeated goblin to the defeated list
+                    defeatedListEl.append(eachGoblinEl);
+                    // stretch goal: change the player image to something stronger-looking every time they hit a new threshold (killed first goblin etc.)
+                    playerImgEl.classList.add('player-killed-one');
+                    playerImgEl.src = './assets/warrior-b.webp';
+                    if (defeatedGoblinsCount === 2) {
+                        playerImgEl.classList.add('player-killed-two');
+                        playerImgEl.src = './assets/warrior-d.webp';
+                    }
+                    if (defeatedGoblinsCount === 3) {
+                        playerImgEl.classList.add('player-killed-three');
+                        playerImgEl.src = './assets/warrior-c.webp';
+                    }
+                    if (defeatedGoblinsCount === 4) {
+                        playerImgEl.classList.add('player-killed-four');
+                        playerImgEl.src = './assets/warrior-e.webp';
+                    }
                 }
-
+                // when user's HP is 0, launch a game over message
                 if (playerHP === 0) {
                     alert('Game Over' + ' You lose' + '!');
                     playerImgEl.classList.add('game-over');
@@ -77,9 +94,9 @@ function displayGoblins() {
                 defeatedNUmberEl.textContent = defeatedGoblinsCount;
                 displayGoblins();
             });
+            
         }
-       
-        // append the 
+        
         goblinListEl.append(eachGoblinEl);
     }
 }
--- render-utils.js
+++ render-utils.js
@@ -1,6 +1,5 @@
 export function renderGoblin(goblinData) {
     const goblinEl = document.createElement('div');
-    // const faceEl = document.createElement('p');
     const faceEl = document.createElement('img');
     const nameEl = document.createElement('p');
     const hpEl = document.createElement('p');
@@ -9,19 +8,14 @@ export function renderGoblin(goblinData) {
     faceEl.classList.add('goblin-img');
 
     nameEl.textContent = goblinData.name;
-    // hpEl.textContent = `HP: ${goblinData.hp}`;
     hpEl.textContent = `HP: ${goblinData.hp < 0 ? 0 : goblinData.hp}`;
     faceEl.src = '../assets/goblin.webp';
 
      //render defeated goblins
     if (goblinData.hp === 0) {
         goblinEl.classList.add('dead');
-        alert('You have killed a goblin!');
     }
-    // hpEl.textContent = `HP: ${goblinData.hp < 0 ? 0 : goblinData.hp}`;
-    // faceEl.textContent = goblinData.hp > 0 ? '👹' : '💀';
-
-      // append that element to the HTML
+    // append that element to the HTML
     goblinEl.append(nameEl, faceEl, hpEl);
     return goblinEl;
 }
